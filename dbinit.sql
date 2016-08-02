73b0c89f9b418ee1ef0572cc9528f41ad196d46f
Dan Daggett <dan@socialgeeks.com>
2016-08-02 03:33:02 +0000

    Update dbinit.sql to add messages table to seasurf database with appropriate permissions for the happy_hxar user.

--- dbinit.sql
+++ dbinit.sql
@@ -1,16 +1,28 @@
 CREATE DATABASE seasurf;
 USE seasurf;
 CREATE TABLE users(
-    id INTEGER,
-    username VARCHAR(30),
-    password VARCHAR(60),
-    admin INT NOT NULL DEFAULT 0
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(30),
+    `password` VARCHAR(60),
+    `admin` INTEGER NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO users (id, username, password, admin) VALUES
-(1, 'admin', '$2y$10$bPTZknRUBCGExuSrGvq8y.pO/pbtOlmuV5NNw.7D07z9HsDFEV8NK', 1);
+INSERT INTO users (username, password, admin) VALUES
+('admin', '$2y$10$bPTZknRUBCGExuSrGvq8y.pO/pbtOlmuV5NNw.7D07z9HsDFEV8NK', 1);
 
-GRANT SELECT,INSERT on seasurf.users to 'happy_hxar'@'localhost' identified by 'yajnidIcIsdedbobgeabcavDafQuioS';
+CREATE TABLE messages(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `message` TEXT,
+    `read` INTEGER NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO messages (message) VALUES
+('message goes here');
+
+GRANT SELECT, INSERT on seasurf.users to 'happy_hxar'@'localhost' identified by 'yajnidIcIsdedbobgeabcavDafQuioS';
+GRANT SELECT, INSERT, UPDATE on seasurf.messages to 'happy_hxar'@'localhost' identified by 'yajnidIcIsdedbobgeabcavDafQuioS';
 
 USE mysql;
 UPDATE user set password=PASSWORD("damjebripujDefDeutAchcokEvbav2") where User='root';
